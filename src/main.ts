7092803b65851d3fb4a3d485ae3f75628963279f
Austin Pierce <Austinentpierce@gmail.com>
2021-09-03 12:26:11 -0400

    added hovering to buttons

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 import './style.css'
+
+const rockButtonPlayerOne = document.querySelector('.player1 i.rock')
+const paperButtonPlayerOne = document.querySelector('.player1 i.paper')
+const scissorsButtonPlaayerOne = document.querySelector('.player1 i.scissors')
+const playerOneNameText = document.querySelector('.player1 h2')
+const playerOneNameInput = document.querySelector('.team1 input')
