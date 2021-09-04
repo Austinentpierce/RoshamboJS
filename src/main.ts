556b8c51ea34dff1ebbcdfc9cf9deae5f78a1b93
Austin Pierce <Austinentpierce@gmail.com>
2021-09-04 11:14:33 -0400

    tidying up design code making sure it looks good for the user

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import './style.css'
 
+let playerOneChoice = ''
+
 const rockButtonPlayerOne = document.querySelector('.player1 i.rock')
 const paperButtonPlayerOne = document.querySelector('.player1 i.paper')
 const scissorsButtonPlaayerOne = document.querySelector('.player1 i.scissors')
 const playerOneNameText = document.querySelector('.player1 h2')
-const playerOneNameInput = document.querySelector('.team1 input')
+const playerOneNameInput = document.querySelector('.player1 input')
+
+let playerTwoChoice = ''
+
+const rockButtonPlayerTwo = document.querySelector('.player2 i.rock')
+const paperButtonPlayerTwo = document.querySelector('.player2 i.rock')
+const scissorsButtonPlaayerTwo = document.querySelector('.player2 i.scissors')
+const playerTwoNameText = document.querySelector('.player2 h2')
+const playerTwoNameInput = document.querySelector('.player2 input')
