5220e5ed162378ef15bcbf28a42e656a000b777e
Austin Pierce <Austinentpierce@gmail.com>
2021-09-04 20:18:02 -0400

    added features for buttons of rock and the outcomes if certain buttons are pushed

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,32 @@ const playerTwoNameText = document.querySelector('.player2 h2')
 const playerTwoNameInput = document.querySelector('.player2 input')
 
 const winner = document.querySelector('footer')
+
+function handleRockButtonPlayerOne(event: Event) {
+  playerOneChoice = 'rock'
+
+  const buttonClicked = event.target
+
+  if (buttonClicked instanceof HTMLButtonElement) {
+    if (playerTwoChoice != '' && playerTwoChoice === 'scissors') {
+      if (winner) {
+        winner.textContent = 'Player 1 Wins!'
+      }
+    }
+  }
+
+if (buttonClicked instanceof HTMLButtonElement) {
+  if (playerTwoChoice != '' && playerTwoChoice === 'paper') {
+    if (winner) {
+      winner.textContent = 'Player 2 Wins!'
+    }
+  }
+}
+if (buttonClicked instanceof HTMLButtonElement) {
+  if (playerTwoChoice != '' && playerTwoChoice === 'rock') {
+    if (winner) {
+      winner.textContent = 'It was a draw'
+    }
+  }
+}
+rockButtonPlayerOne?.addEventListener('click', handleRockButtonPlayerOne)
